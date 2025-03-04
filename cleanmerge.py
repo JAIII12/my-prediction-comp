9bb05ca8ab861bbb64eb5b85c1439a3891fa654f
JAI <sy9534411@gmail.com>
2025-03-05 00:09:10 +0530

    aggregating player data

--- cleanmerge.py
+++ cleanmerge.py
@@ -22,3 +22,18 @@ class Clean_and_Merge(BaseEstimator, TransformerMixin):
             'gk_diving', 'gk_handling', 'gk_kicking', 'gk_positioning',
             'gk_reflexes']
         player_id = df['player_fifa_api_id'].unique()
+
+new_df_list = []
+
+for i in player_id:
+    filtered_df = df[df['player_fifa_api_id'] == i]
+
+    temp_num = filtered_df[numercial_attributes].mean()
+    temp_cat = filtered_df[categorical_attributes].mode().squeeze()
+    temp_cat = temp_cat.fillna("Unknown")  # Handle missing modes
+
+    temp_df = pd.DataFrame({**temp_num.to_dict(), **temp_cat.to_dict()}, index=[0])
+
+    new_df_list.append(temp_df)
+
+new_df = pd.concat(new_df_list, ignore_index=True)
